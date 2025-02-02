6e0a8bfdd7f8da382e4d7004ce86cb9dae2b7cea
Tanishk96 <korgaonkartanishq@gmail.com>
2025-02-02 19:06:31 +0530

    initial setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const colors = require('colors');
+const express = require('express');
+require('dotenv').config(); 
+const PORT = process.env.PORT;
+const app = express();
+const morgan = require('morgan');
+
+// middlewares 
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'))
+
+app.get('/', (req,res)=>{
+    res.send("working fine");
+})
+
+app.listen(PORT,()=>{
+    console.log(`running on port ${process.env.PORT}`.bgMagenta);
+} )
